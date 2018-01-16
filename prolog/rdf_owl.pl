a6b54c8e77da3b3207567b328140970f26cb99ea
Chris Mungall <cjm@berkeleybop.org>
2018-01-16 11:29:32 -0800

    regexes

--- prolog/rdf_owl.pl
+++ prolog/rdf_owl.pl
@@ -1,18 +1,30 @@
 :- module(rdf_owl,
-          [label_atom/2,
+          [regex_str/2,
+           regex_str/3,
+           label_atom/2,
            term_labelify/2,
-
+           row_labelify/2,
+           
            owl_some/3,
            owl_all/3]).
 
 :- use_module(library(semweb/rdf11)).
 :- use_module(library(semweb/rdfs)).
 :- use_module(library(rdf_owl/owl)).
+:- use_module(library(regex)).
 
 :-op(300,xfy,some).
 :-op(300,xfy,all).
 
+regex_str(R,X) :-
+        regex_str(R,X,'').
+regex_str(R,X,Flag) :-
+        X = S^^_,
+        atom_string(S,A),
+        A =~ R/Flag.
+
 label_atom(X,A) :-
+        \+ compound(X),
         label(X,S^^_),
         atom_string(A,S).
 
@@ -35,6 +47,36 @@ term_labelify(T,T-A) :-
         label_atom(T,A),
         !.
 term_labelify(T,T).
+
+%! row_labelify(+Row,?LabeledRow) is det
+%
+% given a row term Foo(V1,V2,...,Vn)
+% add an extra argument for the label for each
+row_labelify(Row,Row2) :-
+        Row =.. [P|Args],
+        rowargs_labelify(Args,Args2),
+        Row2 =.. [P|Args2].
+
+rowargs_labelify([],[]).
+rowargs_labelify([H|T],[H2,Label|T2]) :-
+        (   label_atom(H,Label)
+        ->  true
+        ;   Label=''),
+        compactify_arg(H,H2),
+        rowargs_labelify(T,T2).
+
+compactify_arg(H,H2) :-
+        rdf_global_id(Curie,H),
+        Curie\=H,
+        !,
+        sformat(H2,'~w',[Curie]).
+compactify_arg(Str^^_,H2) :-
+        atom_string(H2,Str),
+        !.
+compactify_arg(H,H).
+
+
+        
 
 %! owl_some(?Restr, ?Property, ?Obj) is nondet.
 %
